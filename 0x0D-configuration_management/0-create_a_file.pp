f9c53ac7c072d687c3fa88efc6cbe9b00248c174
Jacob Biehler <biehlerj@gmail.com>
2018-03-29 16:55:56 -0700

    Adding '>' to all entries

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file{
-  path /tmp/holberton,
+  path => /tmp/holberton,
   content => I love Puppet,
-  owner = www-data,
-  group = www-data,
-  mode = 0744
+  owner => www-data,
+  group => www-data,
+  mode => 0744
 }
