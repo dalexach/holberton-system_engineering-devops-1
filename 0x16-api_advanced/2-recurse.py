5be96ec29f724ce89cf69bab95702804d2395376
Jacob Biehler <biehlerj@gmail.com>
2018-05-04 14:40:29 -0700

    Updating recursion to get rid of recursion error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,17 +9,27 @@ def recurse(subreddit, hot_list=[], after=None):
     if type(subreddit) is not str:
         return None
     sub = subreddit
-    api_url = "https://api.reddit.com/r/{}/hot.json?limit=100".format(sub)
-    if after is None:
-        api_url += "&after={}".format(after)
+    api_url = "https://api.reddit.com/r/{}/hot?after={}".format(sub, after)
     headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(api_url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        hot_posts = response.json()["data"]["children"]
+        after = response.json()["data"]["after"]
+        if after is None:
+            hot_list = titles(hot_posts, len(hot_posts))
+            return hot_list
+        hot_list.append(recurse(subreddit, hot_list, after=after))
+        hot_list = titles(hot_posts, len(hot_posts))
+    else:
         return None
-    response = response.json()
-    after = response.get("data").get("after")
-    hot_posts = response.get("data").get("children")
-    hot_list.extend(map(lambda p: p.get("data").get("title"), hot_posts))
-    if after is None:
-        return hot_list
-    return recurse(subreddit, hot_list, after)
+    return hot_list
+
+
+def titles(hot_list, length, titles_list=[]):
+    """
+    Gets titles of posts from the data
+    """
+    if length == 0:
+        return titles_list
+    titles_list.append(hot_list[length - 1]["data"]["title"])
+    return titles(hot_list, length - 1, titles_list)
