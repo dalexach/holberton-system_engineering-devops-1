80f2f5503c3e04367277cc0d54c6ad3f8880e518
Jacob Biehler <biehlerj@gmail.com>
2018-05-04 01:37:47 -0700

    Querying Reddit API for subreddit subscriber count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers for 
+a given subreddit or 0 if the subreddit is invalid
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    if type(subreddit) is not str:
+        return 0
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'safari:holberton/0.1.0'}
+    response = requests.get(api_url, headers=headers)
+    if response.status_code != 200:
+        return 0
+    return response.json().get("data").get("subscribers")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -8,7 +8,7 @@ This projects deals with getting data from an API and manipulating the type of d
 
 | Files | Description |
 | ----- | ----------- |
-| 0-subs.py | Given an employee ID returns information about that employee's TODO list progress |
+| 0-subs.py | Queries the Reddit API and returns the number of subscribers for a given subreddit |
 | 1-top_ten.py | Extends 0-gather_data_from_an_API.py and exports the data in CSV format |
 | 2-recurse.py | Extends 0-gather_data_from_an_API.py and exports the data in JSON format |
 
