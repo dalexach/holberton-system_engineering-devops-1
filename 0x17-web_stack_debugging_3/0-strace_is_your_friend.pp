68262120a06b40e89191995978398b20af85f12f
Jacob Biehler <biehlerj@gmail.com>
2018-05-15 16:42:34 -0700

    Fixing all .php mentions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes an error with a WordPress Website
 exec { 'fix-wordpress':
-  onlyif  => 'test -e /var/www/html/wp-includes/class-wp-locale.php',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
