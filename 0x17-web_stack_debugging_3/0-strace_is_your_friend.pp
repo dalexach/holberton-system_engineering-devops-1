58689589364186ed772816aed9910f24838e7d26
Jacob Biehler <biehlerj@gmail.com>
2018-05-15 16:01:10 -0700

    Updating script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes an error with a WordPress Website
 exec { 'replace':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-includes/class-wp-locale.php",
   path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   onlyif  => 'test -e /var/www/html/wp-setting.php'
 }
