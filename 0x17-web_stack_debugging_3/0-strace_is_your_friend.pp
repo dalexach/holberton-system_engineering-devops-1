ee6aede6624f4097e2085ee0d7a7ef9e6d99494f
Jacob Biehler <biehlerj@gmail.com>
2018-05-15 16:08:32 -0700

    Fixing only if in the puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'replace':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-includes/class-wp-locale.php",
   path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  onlyif  => 'test -e /var/www/html/wp-setting.php'
+  onlyif  => 'test -e /var/www/html/wwp-includes/class-wp-locale.php'
 }
