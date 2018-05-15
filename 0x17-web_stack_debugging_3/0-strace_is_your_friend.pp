5449f4cc65f0e81195df586ebfadacca42537c18
Jacob Biehler <biehlerj@gmail.com>
2018-05-15 16:42:04 -0700

    Fixing command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes an error with a WordPress Website
-exec { 'replace':
+exec { 'fix-wordpress':
   onlyif  => 'test -e /var/www/html/wp-includes/class-wp-locale.php',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-includes/class-wp-locale.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
